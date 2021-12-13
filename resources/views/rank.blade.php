7985391318788550d34adbc2b6268344497f2ae8
chiukovo <chiuko@celloes.com>
2021-12-13 15:38:37 +0800

    fix

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -87,20 +87,20 @@
                   </tr>
                   @endforeach
                 </table>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2560043137442562"
-                  crossorigin="anonymous"></script>
-                <!-- 94i -->
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-2560043137442562"
-                    data-ad-slot="7062508933"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
               </div>
             </div>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2560043137442562"
+              crossorigin="anonymous"></script>
+            <!-- 94i -->
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-2560043137442562"
+                data-ad-slot="7062508933"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
           </div>
         </main>
         <footer id="footer" class="bg-light" style="height:auto">
